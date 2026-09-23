30f3da553de4a8db0aad047f747af0fef7414a0a
qteunzhuxsyjbnatmnr-lgtm <qteunzhuxsyjbnatmnr@gmail.com>
2026-09-23 09:08:04 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Firebaseの画面に出た設定（公開して問題ない「住所」のようなもの。中身の鍵は firestore.rules でかけている）
+export const firebaseConfig = {
+  apiKey: "AIzaSyBCVUee9FfDG9KoXF8nH3aqUgqDJ_5YvH4",
+  authDomain: "deta-stock.firebaseapp.com",
+  projectId: "deta-stock",
+  storageBucket: "deta-stock.firebasestorage.app",
+  messagingSenderId: "161255005726",
+  appId: "1:161255005726:web:4d717cc3a59fc73058180f",
+};
